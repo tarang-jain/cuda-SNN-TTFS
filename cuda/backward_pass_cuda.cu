0b8d3766702c2a805a22a04e122dad1c9747b382
Vineet Kotariya <36418934+vineetkotariya@users.noreply.github.com>
2021-05-13 01:48:59 +0530

    Minor changes backprop

--- cuda/backward_pass_cuda.cu
+++ cuda/backward_pass_cuda.cu
@@ -1,175 +1,187 @@
-int loss_calc(int * firing_t, int * y_batch,float * delta, int batchSize, int N, int t_max, int * correct){
+// __device__ int min(int a, int b){
+    // return a <= b ? a : b;
+// }
+
+__global__ void loss_calc(int * firing_t, int * y_batch,double * delta, int batchSize, int N, int t_max, int * correct){
 
     int index = blockIdx.y * blockDim.y + threadIdx.y;
     int row = blockIdx.x * blockDim.x + threadIdx.x;
-    __shared__ int min_time[batchSize];
-    vector<vector<int>> target = firing_t;
-    *correct = 0;
+    __shared__ int min_time[100];
+    __shared__ int target[1000];
+    __shared__ int winner[100];
 
     if (index < batchSize && row < N)
     {
+        target[index*N + row] = firing_t[index*N + row];
+        __syncthreads();
         if (row == 0){
-            int winner = 0;
+            winner[index] = 0;
             min_time[index] = t_max;
+
             for (int i = 0; i < N; i++)
             {
                 int time = firing_t[index*N + i];
-                winner = time < min_time[index] ? i : winner;
+                // printf("%d\n", time);
+                winner[index] = time < min_time[index] ? i : winner[index];
                 min_time[index] = time < min_time[index] ? time : min_time[index];
+                // printf("%d\n", min_time[index]);
             }
-            if(winner==y_batch[index]) correct++;
+
+            // printf("%d\n", y_batch[index]);
+            // printf("Okay0");
+
+
+            if(winner[index]==y_batch[index]) atomicAdd(correct, 1);
+            // printf("%d\n", *correct);
+            // printf("Okay");
         }
         __syncthreads();
+        // printf("Yes\n");
 
-        if(min_time[index]==t_max)
+
+        if(min_time[index] == t_max)
         {
-            target[index*N + row] = min_time[index];
+            // printf("No\n");
+            target[index*N + row] = t_max;
+
             if (row == y_batch[index])
             {
-                target[index*N + row] = min_time[index] - gamma_;
+                target[index*N + row] = t_max - gamma_;
             }
         }
         else
         {
-            target[index*N + row] = firing_t[index*N + row];
-            target[index*N + row] = (firing_t[b][i] - min_time) < gamma_ ? (min(min_time + gamma_, tmax)) : firing_t[index*N + row];
-
+            // target[index*N + row] = firing_t[index*N + row];
+            target[index*N + row] = (firing_t[index*N + row] - min_time[index]) < gamma_ ? (min(min_time[index] + gamma_, t_max)) : firing_t[index*N + row];
+            // printf("%d\n", y_batch[index]);
             if (row == y_batch[index])
             {
                 target[index*N + row] = min_time[index];
             }
         }
+        // __syncthreads();
 
-        delta[index * N + row] = target[index * N + row] - firing_t[index * N + row]; //delta = target - firing_t;
+        delta[index * N + row] = double(target[index * N + row] - firing_t[index * N + row]) / double(t_max); //delta = target - firing_t;
+        // printf("%d\t%d\t%f\n", index, row, delta[index*N + row]);
     }
 }
 
-void compute_norm(float * delta, float * norm, int batchSize, int N){
+__global__ void compute_norm(double * delta, double * norm, int batchSize, int N){
 
     int index = blockIdx.y * blockDim.y + threadIdx.y;
     int row = blockIdx.x * blockDim.x + threadIdx.x;
+
     if (index < batchSize && row < N && threadIdx.x == 0){
-        float sum = 0;
-        for(int i = 0; i < blockDim.x; i++) sum += pow(delta[index*N + i], 2);
-        atomicAdd(norm[index], sum);
+        double sum = 0;
+        for(int i = 0; i < blockDim.x; i++){ 
+            sum += delta[index*N + i]*delta[index*N + i];
+
+        atomicAdd(&norm[index], sum);
     }
 }
+}
 
-void grad_norm(float * delta, float * norm, int batchSize, int N){
+__global__ void grad_norm(double * delta, double * norm, int batchSize, int N){
 
     int index = blockIdx.y * blockDim.y + threadIdx.y;
     int row = blockIdx.x * blockDim.x + threadIdx.x;
+
+    norm[index] = sqrt(norm[index]);
 
     if (index < batchSize && row < N){
         delta[index*N + row] /= norm[index];
     }
 }
 
-void batch_bcast_dot(float * delta, bool * fired_before, float * dw_batch){
+__global__ void batch_bcast_dot(double * delta, bool * fired_before, double * dw_batch, int batchSize, int N_in, int N_out){
 
     int index = blockIdx.z * blockDim.z + threadIdx.z;
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-    int b_size = fired_before.size();
-    int Nin    = fired_before[0].size();
-    int Nout   = fired_before[0][0].size();
-
-    for (int b = 0; b < b_size; ++b)
+    if (index < batchSize && row < N_in && col < N_out)
     {
-        for (int i = 0; i < Nin; ++i)
-        {
-            for (int j = 0; j < Nout; ++j)
-            {
-                dw_batch[b][i][j] = delta[b][j]*fired_before[b][i][j];
-            }
-        }
+        // printf("%lf\n", delta[index*N_out + col]);
+        dw_batch[index*N_in*N_out + row*N_out + col] = delta[index*N_out + col]*fired_before[index*N_in*N_out + row*N_out + col];
     }
 }
 
-void reduce_sum3d(const vector<vector<vector<double>>>& dw_batch, vector<vector<double>>& dw){
-    //reduce_sum along axis 0
-    int b_size = dw_batch.size();
-    int Nin    = dw_batch[0].size();
-    int Nout   = dw_batch[0][0].size();
+__global__ void reduce_sum3d(double * dw_batch, double * dw, int batchSize, int N_in, int N_out){
 
-    dw = vector<vector<double>>(Nin, vector<double>(Nout, 0));
+    int index = blockIdx.z * blockDim.z + threadIdx.z;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-    for (int b = 0; b < b_size; ++b)
+    if (index == 0 && row < N_in && col < N_out)
     {
-        for (int i = 0; i < Nin; ++i)
+        for (int b = 0; b < batchSize; ++b)
         {
-            for (int j = 0; j < Nout; ++j)
-            {
-                dw[i][j] += dw_batch[b][i][j];
-            }
+            dw[row*N_out + col] += dw_batch[b*N_in*N_out + row*N_out + col];
         }
-    }
+        // printf("%d\t%d\t%lf\n", row, col, dw[row*N_out + col]);  
+    }  
 }
 
-void batch_multiply(const vector<vector<vector<double>>>& dw_batch, const vector<vector<double>>& w, vector<vector<double>>& delta){
-    int b_size = dw_batch.size();
-    int Nin = w.size();
-    int Nout = w[0].size();
-
-    //vector<vector<vector<double>>> delta_batch(b_size, vector<vector<bool>>(Nin, vector<bool>(Nout, false)));
-    delta = vector<vector<double>>(b_size, vector<double>(Nin, 0));
-
-    for (int b = 0; b < b_size; ++b)
-    {
-        for (int i = 0; i < Nin; ++i)
-        {
-            for (int j = 0; j < Nout; ++j)
-            {
-                //delta_batch[b][i][j] = dw_batch[b][i][j]*dw[i][j];
-                delta[b][i] += dw_batch[b][i][j]*w[i][j];
-            }
-        }
-    }
+__global__ void batch_multiply(double* dw_batch, double* w, double* delta, int batchSize, int N_in, int N_out){
     
+    int index = blockIdx.z * blockDim.z + threadIdx.z;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
 
+    if (index < batchSize && row < N_in && col ==0)
+    {
+        for (int j = 0; j < N_out; ++j)
+        {
+            delta[index*N_in + row] += dw_batch[index*N_in*N_out + row*N_out + j]*w[row*N_out + j];
+        }  
+    }  
+
+}
+
+__global__ void create_fired_before(int * firing_t_in, int * firing_t_out, int batchSize, bool * fired_before, int N_in, int N_out){
+
+    int index = blockIdx.z * blockDim.z + threadIdx.z;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    fired_before[index*N_in*N_out + row*N_out + col] = firing_t_in[index*N_in + row] < firing_t_out[index*N_out + col];    
 }
 
 //backprop_dense(firing_t1, firing_t0, delta2, delta1, w2,  lr2);
-void backprop_dense(const vector<vector<int>>& firing_t1, const vector<vector<int>>& firing_t0, vector<vector<double>>& delta2,
-                    vector<vector<double>>& delta1, vector<vector<double>>& w2, vector<vector<double>>& dw2, int lr, bool calc_delta){
+void backprop_dense(int * firing_t_out, int * firing_t_in, double * delta2,double * delta1, double * w2, double * dw2, double lr, bool calc_delta, int batchSize, int N_in, int N_out){
 
-    int b_size = firing_t0.size();
-    int Nin = w2.size();
-    int Nout = w2[0].size();
-    grad_norm(delta2);
+    double * norm, * dw2_batch;
+    bool * fired_before;
+    cudaMalloc((void **) &norm, sizeof(double)*batchSize);
+    cudaMalloc((void **) &dw2_batch, sizeof(double)*batchSize*N_in*N_out);
+    cudaMalloc((void **) &fired_before, sizeof(bool)*batchSize*N_in*N_out);
 
-    vector<vector<vector<bool>>> fired_before(b_size, vector<vector<bool>>(Nin, vector<bool>(Nout, false)));
-    
-    for (int b = 0; b < b_size; ++b)
-    {
-        for (int i = 0; i < Nin; ++i)
-        {
-            for (int j = 0; j < Nout; ++j)
-            {
-                fired_before[b][i][j] = firing_t0[b][i] < firing_t1[b][j];
-            }
-        }
-    }
-    vector<vector<vector<double>>> dw2_batch(b_size, vector<vector<double>>(Nin, vector<double>(Nout, 0)));
-    //vector<vector<double>> dw2; //(Nin, vector<double>(Nout, 0));
-    batch_bcast_dot(delta2, fired_before, dw2_batch);
-    reduce_sum3d(dw2_batch, dw2);
+    dim3 threadsPerBlock_2D(10, 100);
+    dim3 blocksPerGrid_2D(1, 1);
 
+    blocksPerGrid_2D.x = ceil(double(N_out)/double(threadsPerBlock_2D.x));
+    blocksPerGrid_2D.y = ceil(double(batchSize)/double(threadsPerBlock_2D.y));
 
-    if(calc_delta) batch_multiply(dw2_batch, w2, delta1); //Check if this has to be dw2_batch then reduction
+    compute_norm<<<blocksPerGrid_2D, threadsPerBlock_2D>>>(delta2, norm, batchSize, N_out);
+    grad_norm<<<blocksPerGrid_2D, threadsPerBlock_2D>>>(delta2, norm, batchSize, N_out);
+
+    dim3 threadsPerBlock_3D(8, 16, 8);
+    dim3 blocksPerGrid_3D(1, 1, 1);
+
+    blocksPerGrid_3D.x = ceil(double(N_out)/double(threadsPerBlock_3D.x));
+    blocksPerGrid_3D.y = ceil(double(N_in)/double(threadsPerBlock_3D.y));
+    blocksPerGrid_3D.z = ceil(double(batchSize)/double(threadsPerBlock_3D.z));
+
+    create_fired_before<<<blocksPerGrid_3D, threadsPerBlock_3D>>>(firing_t_in, firing_t_out, batchSize, fired_before, N_in, N_out);
+    batch_bcast_dot<<<blocksPerGrid_3D, threadsPerBlock_3D>>>(delta2, fired_before, dw2_batch, batchSize, N_in, N_out);
+    reduce_sum3d<<<blocksPerGrid_3D, threadsPerBlock_3D>>>(dw2_batch, dw2, batchSize, N_in, N_out);
+
+    if(calc_delta) batch_multiply<<<blocksPerGrid_3D, threadsPerBlock_3D>>>(dw2_batch, w2, delta1, batchSize, N_in, N_out);
 
 }
 
-void update_weights(vector<vector<double>>& w, const vector<vector<double>>& dw, double lr){
-    int Nin = w.size();
-    int Nout = w[0].size();
-
-    for (int i = 0; i < Nin; ++i)
-    {
-        for (int j = 0; j < Nout; ++j)
-        {
-            w[i][j] += lr*dw[i][j];
-        }
-    }
+__global__ void update_weights(double * w, double * dw, double lr, int N_in, int N_out){
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    w[row*N_out + col] += lr*dw[row*N_out + col];
 }
--- cuda/run_cuda.cu
+++ cuda/run_cuda.cu
@@ -5,12 +5,15 @@
 //int n_layers = 2;
 const int img_size   = 28*28;
 const int n_inp      = 28*28;
-const int n_hid      = 1000;
+const int n_hid      = 400;
 const int n_out      = 10;
-const int n_epochs   = 1;
+const int n_epochs   = 30;
 const int batch_size = 100;
-const int n_batches  = n_train/batch_size;
+const int n_batches  = n_train / batch_size;
 const int th_val     = 100;
+const int gamma_     = 3;
+const double lr1        = 0.2;
+const double lr2        = 0.2;
 const int a1 = 0, b1 = 5, a2 = 0, b2 = 50;
 
 #include <stdio.h>
@@ -20,21 +23,41 @@ const int a1 = 0, b1 = 5, a2 = 0, b2 = 50;
 #include "cuda_runtime.h"
 #include "initialization.cpp"
 #include "forward_pass_cuda.cu"
-
+#include "backward_pass_cuda.cu"
 int main(int argc, char** argv){
 
-	float *w1, *w2, *dev_w1, *dev_w2;
+	int *x_batch, * dev_x_batch, *dev_x1_out, *dev_x2_out, *firing_t0, *dev_firing_t0, *dev_firing_t1, *dev_firing_t2, *y_batch, *dev_y_batch;
+	double *w1, *w2, *dev_w1, *dev_w2, *dev_dw1, *dev_dw2, *dev_delta0, *dev_delta1, *dev_delta2, *dev_correct, *dev_lr1, *dev_lr2;
 
-	w1 = (float*) malloc(sizeof(float)*img_size*n_hid);
-    w2 = (float*) malloc(sizeof(float)*n_hid*n_out);
+	w1 = (double*) malloc(sizeof(double)*img_size*n_hid);
+    w2 = (double*) malloc(sizeof(double)*n_hid*n_out);
+	x_batch = (int *) malloc(sizeof(int)*batch_size*n_inp*tmax);
+	firing_t0 = (int *) malloc(sizeof(int)*batch_size*n_inp);
+	y_batch = (int *) malloc(sizeof(int)*batch_size);
+	cudaMalloc((void **) &dev_w1, sizeof(double)*img_size*n_hid);
+    cudaMalloc((void **) &dev_w2, sizeof(double)*n_hid*n_out);
+	cudaMalloc((void **) &dev_dw1, sizeof(double)*img_size*n_hid);
+    cudaMalloc((void **) &dev_dw2, sizeof(double)*n_hid*n_out);
+	cudaMalloc((void **) &dev_x_batch, sizeof(int)*batch_size*n_inp*tmax);
+	cudaMalloc((void **) &dev_x1_out, sizeof(int)*batch_size*n_hid*tmax);
+	cudaMalloc((void **) &dev_x2_out, sizeof(int)*batch_size*n_out*tmax);
+	cudaMalloc((void **) &dev_firing_t0, sizeof(int)*batch_size*n_inp);
+	cudaMalloc((void **) &dev_firing_t1, sizeof(int)*batch_size*n_hid);
+	cudaMalloc((void **) &dev_firing_t2, sizeof(int)*batch_size*n_out);
+	cudaMalloc((void **) &dev_delta0, sizeof(double)*batch_size*n_inp);
+	cudaMalloc((void **) &dev_delta1, sizeof(double)*batch_size*n_hid);
+	cudaMalloc((void **) &dev_delta2, sizeof(double)*batch_size*n_out);
+	cudaMalloc((void **) &dev_correct, sizeof(int));
+	cudaMalloc((void **) &dev_lr1, sizeof(double));
+	cudaMalloc((void **) &dev_lr2, sizeof(double));
+	cudaMalloc((void **) &dev_y_batch, sizeof(int)*batch_size);
 
-	cudaMalloc((void **) &dev_w1, sizeof(float)*img_size*n_hid);
-    cudaMalloc((void **) &dev_w2, sizeof(float)*n_hid*n_out);
-	
 	weight_init_1D(w1, w2, img_size*n_hid, n_hid*n_out);
 
-	cudaMemcpy(dev_w1, w1, sizeof(float)*n_inp*n_hid, cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_w2, w2, sizeof(float)*n_hid*n_out, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_w1, w1, sizeof(double)*n_inp*n_hid, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_w2, w2, sizeof(double)*n_hid*n_out, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_lr1, &lr1, sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_lr2, &lr2, sizeof(double), cudaMemcpyHostToDevice);
 
 	vector<vector<double>> x_train,  x_test;
 	vector<int> y_train, y_test;
@@ -44,65 +67,106 @@ int main(int argc, char** argv){
 	ReadMNIST_label(60000,  y_train,"../train-labels-idx1-ubyte");
 	ReadMNIST_label(10000,  y_test,"../t10k-labels-idx1-ubyte");
 
+
+
 	cout <<"Training the model....\n";
 	//Training:
 	for (int e = 0; e < n_epochs; ++e)
 	{
 		cout <<"epoch: "<<e+1<<" \n";
-		rand_shuffle(x_train, y_train);
+		// rand_shuffle(x_train, y_train);
+
+		int training_acc = 0;
+		int *dev_training_acc;
+
+		cudaMalloc((void **) &dev_training_acc, sizeof(int));
+		cudaMemcpy(dev_training_acc, &training_acc, sizeof(int), cudaMemcpyHostToDevice);
 
 		for (int b = 0; b < n_batches; ++b)
 		{
 			cout<<"batch: "<<b+1<<"\n";
 
-			int *x_batch, *dev_x_batch, *dev_x1_out, *dev_x2_out, *firing_t0, *dev_firing_t0, *dev_firing_t1, *dev_firing_t2;
-			x_batch = (int *) calloc(batch_size*n_inp*tmax, sizeof(int));
-			firing_t0 = (int *) malloc(sizeof(int)*batch_size*n_inp);
-			vector<int> y_batch; // (batch_size, n_out)
-			
-			cudaMalloc((void **) &dev_x_batch, sizeof(int)*batch_size*n_inp*tmax);
-			cudaMalloc((void **) &dev_x1_out, sizeof(int)*batch_size*n_hid*tmax);
-			cudaMalloc((void **) &dev_x2_out, sizeof(int)*batch_size*n_out*tmax);
-			cudaMalloc((void **) &dev_firing_t0, sizeof(int)*batch_size*n_inp);
-			cudaMalloc((void **) &dev_firing_t1, sizeof(int)*batch_size*n_hid);
-			cudaMalloc((void **) &dev_firing_t2, sizeof(int)*batch_size*n_out);
+
+
+
+			memset(x_batch, 0, batch_size*n_inp*tmax);
+
+			cudaMemset(dev_dw1, 0, sizeof(double)*img_size*n_hid);
+			cudaMemset(dev_dw2, 0, sizeof(double)*n_hid*n_out);			
 			cudaMemset(dev_x1_out, 0, sizeof(int)*batch_size*n_hid*tmax);
 			cudaMemset(dev_x1_out, 0, sizeof(int)*batch_size*n_out*tmax);
 
 			batch_spike_encoding_1D(x_train, y_train, x_batch, y_batch, firing_t0, b, batch_size, n_inp);
 
 			cudaMemcpy(dev_x_batch, x_batch, sizeof(int)*batch_size*n_inp*tmax, cudaMemcpyHostToDevice);
+			cudaMemcpy(dev_y_batch, y_batch, sizeof(int)*batch_size, cudaMemcpyHostToDevice);
 			cudaMemcpy(dev_firing_t0, firing_t0, sizeof(int)*batch_size*n_inp, cudaMemcpyHostToDevice);
 
 			batch_dense(dev_x_batch, dev_x1_out, dev_firing_t1, dev_w1, n_inp, n_hid, batch_size, th_val, tmax);
 			batch_dense(dev_x1_out, dev_x2_out, dev_firing_t2, dev_w2, n_hid, n_out, batch_size, th_val, tmax);
-		
 
-	// 		//loss_calc(x2_out, y_batch, grad2);
+			dim3 threadsPerBlock_2D(n_out, batch_size);
+			dim3 blocksPerGrid_2D(1, 1);
 
-	// 		//backprop_dense(grad2, w2, grad1, lr2);
-	// 		//backprop_dense(grad1, w1, grad0, lr1);
 
-	// 		/*
-	// 		//backprop(grad2, w2, grad1, lr2, dw2);
-	// 		//update_weights(w2, dw1, lr2);
-	// 		*/
+			if (batch_size > floor(double(1024)/double(n_out))){
+				threadsPerBlock_2D.y = floor(double(1024)/double(n_out));
+				blocksPerGrid_2D.y = ceil(double(batch_size)/double(threadsPerBlock_2D.y));
+			}
 
-	        free(x_batch);
-			free(firing_t0);
-			cudaFree(dev_x_batch);
-	        cudaFree(dev_x1_out);
-	        cudaFree(dev_x2_out);
-			cudaFree(dev_firing_t0);
-			cudaFree(dev_firing_t1);
-			cudaFree(dev_firing_t2);
+
+			loss_calc<<<blocksPerGrid_2D, threadsPerBlock_2D>>>(dev_firing_t2, dev_y_batch, dev_delta2, batch_size, n_out, tmax, dev_training_acc);
+
+			cudaMemcpy(&training_acc, dev_training_acc, sizeof(int), cudaMemcpyDeviceToHost);
+
+			cout<<"\t\ttrain acc = "<<training_acc<<"/"<<((b+1)*batch_size)<<" = "<<(training_acc/(1.0*(b+1)*batch_size))<<"\n";
+
+			backprop_dense(dev_firing_t2, dev_firing_t1, dev_delta2, dev_delta1, dev_w2, dev_dw2, lr2, true, batch_size, n_hid, n_out);
+
+			if (n_hid > floor(double(1024)/double(n_out))){
+				threadsPerBlock_2D.y = floor(double(1024)/double(n_out));
+				blocksPerGrid_2D.y = ceil(double(n_hid)/double(threadsPerBlock_2D.y));
+			}
+
+			update_weights<<<blocksPerGrid_2D, threadsPerBlock_2D>>>(dev_w2, dev_dw2, lr2, n_hid, n_out);
+
+			backprop_dense(dev_firing_t1, dev_firing_t0, dev_delta1, dev_delta0, dev_w1, dev_dw1, lr1, false, batch_size, n_inp, n_hid);
+
+			threadsPerBlock_2D.x = n_hid;
+			threadsPerBlock_2D.y = n_inp;
+
+			if (n_inp > floor(double(1024)/double(n_hid))){
+				threadsPerBlock_2D.y = floor(double(1024)/double(n_hid));
+				blocksPerGrid_2D.y = ceil(double(n_inp)/double(threadsPerBlock_2D.y));
+			}
+
+			update_weights<<<blocksPerGrid_2D, threadsPerBlock_2D>>>(dev_w1, dev_dw1, lr1, n_inp, n_hid);
+
 		}
 	}
 
-    free(w1);
+	free(x_batch);
+	free(firing_t0);
+	free(w1);
 	free(w2);
+	cudaFree(dev_x_batch);
+	cudaFree(dev_x1_out);
+	cudaFree(dev_x2_out);
+	cudaFree(dev_firing_t0);
+	cudaFree(dev_firing_t1);
+	cudaFree(dev_firing_t2);
 	cudaFree(dev_w1);
 	cudaFree(dev_w2);
+	cudaFree(dev_dw1);
+	cudaFree(dev_dw2);
+	cudaFree(dev_dw2);
+	cudaFree(dev_delta0);
+	cudaFree(dev_delta1);
+	cudaFree(dev_delta2);
+	cudaFree(dev_correct);
+	cudaFree(dev_lr1);
+	cudaFree(dev_lr2);
+
 	return 0;
 	
 }
